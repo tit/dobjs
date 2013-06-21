4d3c6a14829b7f3d1f621e574169f4782f338255
Sergey Blohin <sblohin@yandex.ru>
2013-06-22 01:38:54 +0400

    Sugar

--- dob.js
+++ dob.js
@@ -1,213 +1,242 @@
-if (typeof dob_settings.cookie_day_expires == 'undefined') {
-    dob_settings.cookie_day_expires = 3;
-}
+(function () {
+  function declOfNum(number, titles)
+  {
+    cases = [2, 0, 1, 1, 1, 2];
+    return titles[ (number%100>4 && number%100<20)? 2 : cases[(number%10<5)?number%10:5] ];
+  }
 
-function dob_setCookie(c_name, value, exdays) {
+  if ('undefined' == typeof dob_settings.cookie_day_expires) dob_settings.cookie_day_expires = 3;
+  if ('undefined' == typeof dob_settings.background_color) dob_settings.background_color = 'red';
+  if ('undefined' == typeof dob_settings.language) dob_settings.language = 'en';
+
+  dob_settings.i18n = {
+    en: {
+      notification_text: 'For correctly view the site, update or change your browser',
+      close_text: 'Close for ' + dob_settings.cookie_day_expires + ' days'
+    },
+    ru: {
+      notification_text: 'Для корректного просмотра сайта обновите или смените ваш браузер',
+      close_text: 'Закрыть на ' + dob_settings.cookie_day_expires + ' ' + declOfNum(dob_settings.cookie_day_expires, ['день', 'дня', 'дней'])
+    }
+  };
+
+  function setCookie(c_name, value, exdays) {
     var exdate = new Date();
     exdate.setDate(exdate.getDate() + exdays);
     var c_value = escape(value) + ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
     document.cookie = c_name + "=" + c_value;
-}
+  }
 
-function dob_getCookie(c_name) {
+  function getCookie(c_name) {
     var c_value = document.cookie;
     var c_start = c_value.indexOf(" " + c_name + "=");
     if (c_start == -1) {
-        c_start = c_value.indexOf(c_name + "=");
+      c_start = c_value.indexOf(c_name + "=");
     }
     if (c_start == -1) {
-        c_value = null;
+      c_value = null;
     }
     else {
-        c_start = c_value.indexOf("=", c_start) + 1;
-        var c_end = c_value.indexOf(";", c_start);
-        if (c_end == -1) {
-            c_end = c_value.length;
-        }
-        c_value = unescape(c_value.substring(c_start, c_end));
+      c_start = c_value.indexOf("=", c_start) + 1;
+      var c_end = c_value.indexOf(";", c_start);
+      if (c_end == -1) {
+        c_end = c_value.length;
+      }
+      c_value = unescape(c_value.substring(c_start, c_end));
     }
     return c_value;
-}
+  }
 
-function dob_close() {
-    dob_setCookie('dob_ignore', 'true', dob_settings.cookie_day_expires);
+  function banner_close() {
+    setCookie('dob_ignore', 'true', dob_settings.cookie_day_expires);
     document.body.removeChild(browser_update_parent_div);
-}
+  }
 
-var BrowserDetect = {
+  var BrowserDetect = {
     init: function () {
-        this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
-        this.version = this.searchVersion(navigator.userAgent)
-            || this.searchVersion(navigator.appVersion)
-            || "an unknown version";
-        this.OS = this.searchString(this.dataOS) || "an unknown OS";
-        this.device = this.searchString(this.dataDevice) || "an unknown Device";
+      this.browser = this.searchString(this.dataBrowser) || "An unknown browser";
+      this.version = this.searchVersion(navigator.userAgent)
+          || this.searchVersion(navigator.appVersion)
+          || "an unknown version";
+      this.OS = this.searchString(this.dataOS) || "an unknown OS";
+      this.device = this.searchString(this.dataDevice) || "an unknown Device";
     },
     searchString: function (data) {
-        for (var i = 0; i < data.length; i++) {
-            var dataString = data[i].string;
-            var dataProp = data[i].prop;
-            this.versionSearchString = data[i].versionSearch || data[i].identity;
-            if (dataString) {
-                if (dataString.indexOf(data[i].subString) != -1)
-                    return data[i].identity;
-            }
-            else if (dataProp)
-                return data[i].identity;
+      for (var i = 0; i < data.length; i++) {
+        var dataString = data[i].string;
+        var dataProp = data[i].prop;
+        this.versionSearchString = data[i].versionSearch || data[i].identity;
+        if (dataString) {
+          if (dataString.indexOf(data[i].subString) != -1)
+            return data[i].identity;
         }
+        else if (dataProp)
+          return data[i].identity;
+      }
     },
     searchVersion: function (dataString) {
-        var index = dataString.indexOf(this.versionSearchString);
-        if (index == -1) return;
-        return parseFloat(dataString.substring(index + this.versionSearchString.length + 1));
+      var index = dataString.indexOf(this.versionSearchString);
+      if (index == -1) return;
+      return parseFloat(dataString.substring(index + this.versionSearchString.length + 1));
     },
     dataBrowser: [
-        {
-            string: navigator.userAgent,
-            subString: "RockMelt",
-            identity: "RockMelt"
-        },
-        {
-            string: navigator.userAgent,
-            subString: "Chrome",
-            identity: "Chrome"
-        },
-        { string: navigator.userAgent,
-            subString: "OmniWeb",
-            versionSearch: "OmniWeb/",
-            identity: "OmniWeb"
-        },
-        {
-            string: navigator.vendor,
-            subString: "Apple",
-            identity: "Safari",
-            versionSearch: "Version"
-        },
-        {
-            prop: window.opera,
-            identity: "Opera"
-        },
-        {
-            string: navigator.vendor,
-            subString: "iCab",
-            identity: "iCab"
-        },
-        {
-            string: navigator.vendor,
-            subString: "KDE",
-            identity: "Konqueror"
-        },
-        {
-            string: navigator.userAgent,
-            subString: "Firefox",
-            identity: "Firefox"
-        },
-        {
-            string: navigator.vendor,
-            subString: "Camino",
-            identity: "Camino"
-        },
-        {   // for newer Netscapes (6+)
-            string: navigator.userAgent,
-            subString: "Netscape",
-            identity: "Netscape"
-        },
-        {
-            string: navigator.userAgent,
-            subString: "MSIE",
-            identity: "IE",
-            versionSearch: "MSIE"
-        },
-        {
-            string: navigator.userAgent,
-            subString: "Gecko",
-            identity: "Mozilla",
-            versionSearch: "rv"
-        },
-        {
-            // for older Netscapes (4-)
-            string: navigator.userAgent,
-            subString: "Mozilla",
-            identity: "Netscape",
-            versionSearch: "Mozilla"
-        }
+      {
+        string: navigator.userAgent,
+        subString: "RockMelt",
+        identity: "RockMelt"
+      },
+      {
+        string: navigator.userAgent,
+        subString: "Chrome",
+        identity: "Chrome"
+      },
+      { string: navigator.userAgent,
+        subString: "OmniWeb",
+        versionSearch: "OmniWeb/",
+        identity: "OmniWeb"
+      },
+      {
+        string: navigator.vendor,
+        subString: "Apple",
+        identity: "Safari",
+        versionSearch: "Version"
+      },
+      {
+        prop: window.opera,
+        identity: "Opera"
+      },
+      {
+        string: navigator.vendor,
+        subString: "iCab",
+        identity: "iCab"
+      },
+      {
+        string: navigator.vendor,
+        subString: "KDE",
+        identity: "Konqueror"
+      },
+      {
+        string: navigator.userAgent,
+        subString: "Firefox",
+        identity: "Firefox"
+      },
+      {
+        string: navigator.vendor,
+        subString: "Camino",
+        identity: "Camino"
+      },
+      {   // for newer Netscapes (6+)
+        string: navigator.userAgent,
+        subString: "Netscape",
+        identity: "Netscape"
+      },
+      {
+        string: navigator.userAgent,
+        subString: "MSIE",
+        identity: "IE",
+        versionSearch: "MSIE"
+      },
+      {
+        string: navigator.userAgent,
+        subString: "Gecko",
+        identity: "Mozilla",
+        versionSearch: "rv"
+      },
+      {
+        // for older Netscapes (4-)
+        string: navigator.userAgent,
+        subString: "Mozilla",
+        identity: "Netscape",
+        versionSearch: "Mozilla"
+      }
     ],
     dataOS: [
-        {
-            string: navigator.platform,
-            subString: "Win",
-            identity: "Windows"
-        },
-        {
-            string: navigator.platform,
-            subString: "Mac",
-            identity: "Mac"
-        },
-        {
-            string: navigator.platform,
-            subString: "Linux",
-            identity: "Linux"
-        },
-        {
-            string: navigator.userAgent,
-            subString: "iOS",
-            identity: "iOS"
-        }
+      {
+        string: navigator.platform,
+        subString: "Win",
+        identity: "Windows"
+      },
+      {
+        string: navigator.platform,
+        subString: "Mac",
+        identity: "Mac"
+      },
+      {
+        string: navigator.platform,
+        subString: "Linux",
+        identity: "Linux"
+      },
+      {
+        string: navigator.userAgent,
+        subString: "iOS",
+        identity: "iOS"
+      }
     ],
     dataDevice: [
-        {
-            string: navigator.platform,
-            subString: "iPad",
-            identity: "iPad"
-        },
-        {
-            string: navigator.platform,
-            subString: "iPhone",
-            identity: "iPhone"
-        }
+      {
+        string: navigator.platform,
+        subString: "iPad",
+        identity: "iPad"
+      },
+      {
+        string: navigator.platform,
+        subString: "iPhone",
+        identity: "iPhone"
+      }
     ]
 
-};
-BrowserDetect.init();
+  };
+  BrowserDetect.init();
 
-if ('true' != dob_getCookie('dob_ignore')) {
+  if ('true' != getCookie('dob_ignore')) {
     if (dob_settings.browsers[BrowserDetect.browser.toLowerCase()] > BrowserDetect.version) {
-        var browser_update_parent_div = document.createElement('div');
-        browser_update_parent_div.style.top = '0';
-        browser_update_parent_div.style.left = '0';
-        browser_update_parent_div.style.width = '100%';//Math.max(document.body.clientWidth, document.body.offsetWidth, document.body.scrollWidth);
-        browser_update_parent_div.style.height = '50px';
-        browser_update_parent_div.style.position = 'absolute';
-        browser_update_parent_div.style.opacity = '0.9';
-        browser_update_parent_div.style.backgroundColor = 'red';
-        document.body.appendChild(browser_update_parent_div);
+      var elements = [];
 
-        var browser_update_child_table = document.createElement('table');
-        browser_update_child_table.style.verticalAlign = 'baseline';
-        browser_update_child_table.style.textAlign = 'center';
-        browser_update_child_table.style.width = '100%';
-        browser_update_child_table.style.height = '100%';
-        browser_update_child_table.style.opacity = '1';
-        browser_update_child_table.style.color = 'whitesmoke';
-        browser_update_child_table.style.fontWeight = 'bold';
-        browser_update_child_table.style.fontFamily = 'sans-serif';
-        browser_update_parent_div.appendChild(browser_update_child_table);
+      // div
+      elements[0] = document.createElement('div');
+      elements[0].style.top = '0';
+      elements[0].style.left = '0';
+      elements[0].style.width = '100%';
+      elements[0].style.height = '50px';
+      elements[0].style.position = 'absolute';
+      elements[0].style.opacity = '0.9';
+      elements[0].style.backgroundColor = dob_settings.background_color;
 
-        var browser_update_child_tr = document.createElement('tr');
-        browser_update_child_table.appendChild(browser_update_child_tr);
+      // table
+      elements[1] = document.createElement('table');
+      elements[1].style.verticalAlign = 'baseline';
+      elements[1].style.textAlign = 'center';
+      elements[1].style.width = '100%';
+      elements[1].style.height = '100%';
+      elements[1].style.opacity = '1';
+      elements[1].style.color = 'whitesmoke';
+      elements[1].style.fontWeight = 'bold';
+      elements[1].style.fontFamily = 'sans-serif';
 
-        var browser_update_child_td_text = document.createElement('td');
-        browser_update_child_td_text.innerText = 'Для корректного просмотра сайта обновите или смените ваш браузер.';
-        browser_update_child_tr.appendChild(browser_update_child_td_text);
+      // tr
+      elements[2] = document.createElement('tr');
 
-        var browser_update_child_td_close = document.createElement('td');
-        browser_update_child_tr.appendChild(browser_update_child_td_close);
+      // td
+      elements[3] = document.createElement('td');
+      elements[3].innerText = dob_settings.i18n[dob_settings.language].notification_text;
 
-        var browser_update_child_a_close = document.createElement('a');
-        browser_update_child_a_close.style.color = 'whitesmoke';
-        browser_update_child_a_close.innerText = 'Закрыть';
-        browser_update_child_a_close.setAttribute('href', '#');
-        browser_update_child_a_close.setAttribute('onclick', 'dob_close()');
-        browser_update_child_td_close.appendChild(browser_update_child_a_close);
+      // td
+      elements[4] = document.createElement('td');
+
+      // a
+      elements[5] = document.createElement('a');
+      elements[5].style.color = 'whitesmoke';
+      elements[5].innerText = dob_settings.i18n[dob_settings.language].close_text;
+      elements[5].setAttribute('href', '#');
+      elements[5].setAttribute('onclick', 'dob_close()');
+
+      document.body.appendChild(elements[0]);
+      elements[0].appendChild(elements[1]);
+      elements[1].appendChild(elements[2]);
+      elements[2].appendChild(elements[3]);
+      elements[2].appendChild(elements[4]);
+      elements[4].appendChild(elements[5]);
     }
-}
+  }
+
+}());
